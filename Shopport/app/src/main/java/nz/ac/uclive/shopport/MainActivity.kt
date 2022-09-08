4627231684d3b627b8dbdfcf516d6a4ff86083ec
Swapnil Bhagat <sbh94@uclive.ac.nz>
2022-09-08 17:26:10 +1200

    Init - Added base for app: bottom nav bar + top bar. Additions now only need to be made to the different screens.

---
+++ Shopport/app/src/main/java/nz/ac/uclive/shopport/MainActivity.kt
@@ -0,0 +1,85 @@
+package nz.ac.uclive.shopport
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import nz.ac.uclive.shopport.ui.theme.ShopportTheme
+import java.util.*
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Shopport()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Shopport() {
+    ShopportTheme {
+        val tabs = remember { ShopportTabs.values() }
+        val navController = rememberNavController()
+        Scaffold(
+            topBar = { ShopportAppBar()},
+            bottomBar = { ShopportBottomBar(navController = navController, tabs) }
+        ) { innerPaddingModifier ->
+            NavigationHost(
+                navController = navController,
+                modifier = Modifier.padding(innerPaddingModifier)
+            )
+        }
+    }
+}
+
+@Composable
+fun ShopportBottomBar(navController: NavController, tabs: Array<ShopportTabs>) {
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route ?: ShopportTabs.GIFTLIST.route
+
+    val routes = remember { ShopportTabs.values().map { it.route } }
+    if (currentRoute in routes) {
+        BottomAppBar {
+            tabs.forEach { tab ->
+                NavigationBarItem(
+                    icon = { Icon(painterResource(tab.icon), contentDescription = null) },
+                    label = { Text(stringResource(tab.title).uppercase(Locale.getDefault())) },
+                    selected = currentRoute == tab.route,
+                    onClick = {
+                        if (tab.route != currentRoute) {
+                            navController.navigate(tab.route) {
+                                popUpTo(navController.graph.startDestinationId) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        }
+                    },
+                    alwaysShowLabel = false,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AllPreview() {
+    Shopport()
+}
---
+++ Shopport/app/src/main/java/nz/ac/uclive/shopport/Navigation.kt
@@ -0,0 +1,110 @@
+package nz.ac.uclive.shopport
+
+import android.annotation.SuppressLint
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+private object ShopportDestinations {
+    const val WISHLIST_ROUTE = "wishlist"
+    const val GIFTLIST_ROUTE = "giftlist"
+    const val EXPLORE_ROUTE = "explore"
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NavigationHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(navController = navController, startDestination = ShopportDestinations.GIFTLIST_ROUTE) {
+        composable(ShopportTabs.WISHLIST.route) {
+            Text(text = "Wishlist", modifier = modifier)
+
+//            Scaffold(
+//                floatingActionButton = {
+//                    FloatingActionButton(
+//                        onClick = { /*TODO*/ },
+//                        modifier = modifier.size(56.dp),
+//                        containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+//                    ) {
+//                        Icon(
+//                            imageVector = Icons.Default.Edit,
+//                            contentDescription =  null,
+//                        )
+//                    }
+//                }
+//            ) {
+//                Text(text = "Wishlist", modifier = modifier)
+//            }
+
+        }
+        composable(ShopportTabs.GIFTLIST.route) {
+            Text(text = "Giftlist", modifier = modifier)
+        }
+        composable(ShopportTabs.EXPLORE.route) {
+            Text(text = "Explore", modifier = modifier)
+        }
+    }
+}
+
+@Composable
+fun ShopportAppBar() {
+    TopAppBar(
+        elevation = 0.dp,
+        modifier = Modifier.height(120.dp),
+        backgroundColor = MaterialTheme.colorScheme.background,
+    ) {
+        Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.CenterVertically),
+                painter = painterResource(id = R.drawable.ic_shopport_logo),
+                contentDescription = null
+            )
+            IconButton(
+                modifier = Modifier.align(Alignment.CenterVertically),
+                onClick = { /* todo */ }
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Settings,
+                    contentDescription = stringResource(R.string.settings)
+                )
+            }
+        }
+    }
+}
+
+enum class ShopportTabs(
+    @StringRes val title: Int,
+    @DrawableRes val icon: Int,
+    val route: String
+) {
+    WISHLIST(R.string.wishlist, R.drawable.ic_wishlist, ShopportDestinations.WISHLIST_ROUTE),
+    GIFTLIST(R.string.giftlist, R.drawable.ic_gift, ShopportDestinations.GIFTLIST_ROUTE),
+    EXPLORE(R.string.explore, R.drawable.ic_explore, ShopportDestinations.EXPLORE_ROUTE)
+}
+
+@Preview
+@Composable
+fun ShopportAppBarPreview() {
+    ShopportAppBar()
+}
