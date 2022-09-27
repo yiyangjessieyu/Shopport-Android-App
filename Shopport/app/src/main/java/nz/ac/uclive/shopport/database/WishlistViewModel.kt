9ad4cc59304d69f24eb27b662dcbd9e766555738
Yiyang (Jessie) Yu <yyu69@uclive.ac.nz>
2022-09-27 18:51:36 +1300

    start of notification tutoroial with no impact on app yet

--- Shopport/app/src/main/java/nz/ac/uclive/shopport/database/WishlistViewModel.kt
+++ Shopport/app/src/main/java/nz/ac/uclive/shopport/database/WishlistViewModel.kt
@@ -1,6 +1,12 @@
 package nz.ac.uclive.shopport.database
 
 import android.app.Application
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -12,6 +18,7 @@ class WishlistViewModel (application: Application): AndroidViewModel(application
     val wishListItems: LiveData<List<WishListItem>>
 
     init {
+        createNotificationChannel(context = LocalContext.current)
         val wishListDao = ShopportDatabase.getInstance(application).wishListItemDao()
         shopportRepository = ShopportRepository(wishListDao)
 
@@ -45,6 +52,25 @@ class WishlistViewModel (application: Application): AndroidViewModel(application
 
 }
 
+private fun createNotificationChannel(context: Context) {
+    // Create the NotificationChannel, but only on API 26+
+    // This is because the NotificationChannel class is new and not in the support library
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val name = "NotificationChannelName"
+        val descriptionText = "NotificationDescriptionText"
+        val important = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel("CHANNEL_ID", name, important).apply {
+            description = descriptionText
+        }
+
+        // Register the channel
+        val notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+
+        // tutorial here: https://www.youtube.com/watch?v=bnMncU3uw_o
+    }
+
+}
 
 class ShopportViewModelFactory(val application: Application) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
